631903ac54f6b140c238bbfe01626c4d53584bc9
Bing Chou <virtuallybing@gmail.com>
2011-06-16 09:15:00 -0600

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Gsd::Application.config.secret_token = 'f21e3240aa88dc48b702f629a51a4fab68087bcf40a9b913bbd21d78255d792107244c8b452af503b07e1f206d1761609d31201bbbf11584c862d3ea6ca1219b'
